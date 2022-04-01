784788f658103fe7ccca4bf50df3995612a13651
Guy Wicks <guy.wicks@gmail.com>
2022-04-01 22:14:06 +0100

    refactoring entities

--- app/routers/items.py
+++ app/routers/items.py
@@ -2,17 +2,21 @@ from fastapi import APIRouter, Path, Query, Header, status, Depends
 from typing import List, Optional, FrozenSet
 from sqlalchemy.orm import Session
 
-#from database.connection import SessionLocal, engine
-
-from ..database import schemas, crud
+from ..database import crud
+from ..models import user, item
 from ..database.connection import get_db
 
 router = APIRouter()
 
-# Dependency
+
+@router.post("/users/{user_id}/items/", response_model=item.Item)
+def create_item_for_user(
+    user_id: int, item: item.ItemCreate, db: Session = Depends(get_db)
+):
+    return crud.create_user_item(db=db, item=item, user_id=user_id)
 
 
-@router.get("/items/", response_model=List[schemas.Item])
+@router.get("/items/", response_model=List[item.Item])
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
--- app/routers/users.py
+++ app/routers/users.py
@@ -2,27 +2,28 @@ from fastapi import APIRouter, Path, Query, Header, status, Depends, HTTPExcepti
 from typing import List, Optional, FrozenSet
 from sqlalchemy.orm import Session
 
-from ..database import schemas, crud
+from ..database import crud
+from ..models import user
 from ..database.connection import get_db
 
 router = APIRouter()
 
 
-@router.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+@router.post("/users/", response_model=user.User)
+def create_user(user: user.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
 
 
-@router.get("/users/", response_model=List[schemas.User])
+@router.get("/users/", response_model=List[user.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
-@router.get("/users/{user_id}", response_model=schemas.User)
+@router.get("/users/{user_id}", response_model=user.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
@@ -30,8 +31,3 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
-@router.post("/users/{user_id}/items/", response_model=schemas.Item)
-def create_item_for_user(
-    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-):
-    return crud.create_user_item(db=db, item=item, user_id=user_id)
